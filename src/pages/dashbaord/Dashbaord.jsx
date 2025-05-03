923e443d120acc3880255ba4821546554e228b8d
unknown <irfanshalu3@gmail.com>
2025-05-04 04:05:31 +0530

    image add

--- src/pages/dashbaord/Dashbaord.jsx
+++ src/pages/dashbaord/Dashbaord.jsx
@@ -3,13 +3,13 @@ import Graph from "./components/Graph";
 
 
 const products = [
-  { name: "Spanish chair", image:"872321ed52fee839cde254080f1eaee494dccbd3", brandName: "Gucci", cutPrice: "$230", price: "$230", rating: "2.3" },
-  { name: "Modern Sofa", brandName: "Ikea", cutPrice: "$500", price: "$450", rating: "4.5" },
-  { name: "Wooden Table", brandName: "Ashley", cutPrice: "$300", price: "$280", rating: "4.0" },
-  { name: "Leather Recliner", brandName: "La-Z-Boy", cutPrice: "$700", price: "$650", rating: "4.8" },
-  { name: "Dining Set", brandName: "Wayfair", cutPrice: "$1200", price: "$1100", rating: "4.6" },
-  { name: "Office Chair", brandName: "Herman Miller", cutPrice: "$900", price: "$850", rating: "4.9" },
-  { name: "Bed Frame", brandName: "Zinus", cutPrice: "$400", price: "$350", rating: "4.2" }
+  { name: "Spanish chair", image: "872321ed52fee839cde254080f1eaee494dccbd3", brandName: "Gucci", cutPrice: "$230", price: "$230", rating: "2.3" },
+  { name: "Modern Sofa", image: "872321ed52fee839cde254080f1eaee494dccbd3", brandName: "Ikea", cutPrice: "$500", price: "$450", rating: "4.5" },
+  { name: "Wooden Table", image: "872321ed52fee839cde254080f1eaee494dccbd3", brandName: "Ashley", cutPrice: "$300", price: "$280", rating: "4.0" },
+  { name: "Leather Recliner", image: "872321ed52fee839cde254080f1eaee494dccbd3", brandName: "La-Z-Boy", cutPrice: "$700", price: "$650", rating: "4.8" },
+  { name: "Dining Set", image: "872321ed52fee839cde254080f1eaee494dccbd3", brandName: "Wayfair", cutPrice: "$1200", price: "$1100", rating: "4.6" },
+  { name: "Office Chair", image: "872321ed52fee839cde254080f1eaee494dccbd3", brandName: "Herman Miller", cutPrice: "$900", price: "$850", rating: "4.9" },
+  { name: "Bed Frame", image: "872321ed52fee839cde254080f1eaee494dccbd3", brandName: "Zinus", cutPrice: "$400", price: "$350", rating: "4.2" }
 ];
 
 const Dashboard = () => {
